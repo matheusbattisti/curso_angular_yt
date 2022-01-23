8d549c01f7946ccb11d355db5c07307d01049e4f
Matheus Battisti <matheusbudkewicz@gmail.com>
2022-01-23 14:47:04 -0300

    finalizando projeto

--- moments/src/app/components/pages/home/home.component.ts
+++ moments/src/app/components/pages/home/home.component.ts
@@ -6,31 +6,44 @@ import { Moment } from 'src/app/Moment';
 
 import { environment } from 'src/environments/environment';
 
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  allMoments: Moment[] = [];
   moments: Moment[] = [];
   baseApiUrl = environment.baseApiUrl;
+
+  searchTerm: string = '';
+  faSearch = faSearch;
 
   constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
     this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
+      });
+
+      this.allMoments = items.data;
       this.moments = items.data;
     });
   }
 
-  searchMoment(event: Event): void {
-    const search = event.target as HTMLInputElement;
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
 
-    console.log(this.moments);
-
-    this.moments = this.moments.filter((moment) => {
-      console.log(moment.title);
-      return moment.title.includes(search.value);
-    });
+    this.moments = this.allMoments.filter((moment) =>
+      moment.title.toLowerCase().includes(value)
+    );
   }
 }
